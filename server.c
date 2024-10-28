5110ebce120f66d4519df12f6b168ad6b983e97e
Yutosaki <sasayu8190@gmail.com>
2024-10-28 11:29:20 +0900

    refactor: did declare and initialize at the same time, so divide

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sasakiyuto <sasakiyuto@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 21:32:53 by sasakiyuto        #+#    #+#             */
-/*   Updated: 2024/10/28 10:47:03 by sasakiyuto       ###   ########.fr       */
+/*   Updated: 2024/10/28 11:27:23 by sasakiyuto       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@
 
 void	handle_signal(int sig)
 {
-	static int	bit_count = 0;
-	static char	current_char = 0;
+	static int	bit_count;
+	static char	current_char;
 
+	bit_count = 0;
+	current_char = 0;
 	if (sig == SIGUSR1)
 		current_char |= (1 << bit_count);
 	bit_count++;
