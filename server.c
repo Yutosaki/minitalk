60d76cd6fadc3a2c415263804462a74f1f6df9bf
Yutosaki <sasayu8190@gmail.com>
2024-11-03 20:06:36 +0900

    fix: change static not initiarize every time it's called

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sasakiyuto <sasakiyuto@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 21:32:53 by sasakiyuto        #+#    #+#             */
-/*   Updated: 2024/10/28 11:27:23 by sasakiyuto       ###   ########.fr       */
+/*   Updated: 2024/11/03 20:05:29 by sasakiyuto       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,24 @@
 
 void	handle_signal(int sig)
 {
-	static int	bit_count;
-	static char	current_char;
+	static int	bit_count = 0;
+	static char	current_char = 0;
 
-	bit_count = 0;
-	current_char = 0;
 	if (sig == SIGUSR1)
 		current_char |= (1 << bit_count);
 	bit_count++;
 	if (bit_count == 8)
 	{
 		write(1, &current_char, 1);
+		ssize_t bytes_written = write(1, &current_char, 1);
+        if (bytes_written == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        } else if (bytes_written != 1) {
+			ft_printf("bytes_written = %ld\n", bytes_written); // もっと詳しくエラーを表示する
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
 		bit_count = 0;
 		current_char = 0;
 	}
