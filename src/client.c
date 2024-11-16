5085f5c4c3fe23f197a87cb67568cc455cef4f2f
Yutosaki <sasayu8190@gmail.com>
2024-11-16 16:32:56 +0900

    feat: change usleep to 2000. don't use sleep coz it recieve only unsigned int

---
+++ src/client.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yutsasak <yutsasak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 21:32:56 by sasakiyuto        #+#    #+#             */
+/*   Updated: 2024/11/16 16:30:53 by yutsasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "libft.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	send_char(int pid, char c)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c & (1 << i))
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				perror("kill");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				perror("kill");
+				exit(EXIT_FAILURE);
+			}
+		}
+		i++;
+		usleep(2000);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t	pid;
+	char	*message;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <pid> <string>\n", argv[0]);
+		return (1);
+	}
+	message = argv[2];
+	pid = atoi(argv[1]);
+	while (*message)
+	{
+		send_char(pid, *message);
+		message++;
+	}
+	send_char(pid, '\0');
+	return (0);
+}
---
+++ src/server.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yutsasak <yutsasak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 21:32:53 by sasakiyuto        #+#    #+#             */
+/*   Updated: 2024/11/13 20:49:14 by yutsasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+volatile sig_atomic_t	g_received = 0;
+
+void	handle_signal(int sig)
+{
+	static int	bit_count = 0;
+	static char	current_char = 0;
+
+	if (sig == SIGUSR1)
+		current_char |= (1 << bit_count);
+	bit_count++;
+	if (bit_count == 8)
+	{
+		write(1, &current_char, 1);
+		bit_count = 0;
+		current_char = 0;
+	}
+}
+
+int	main(void)
+{
+	int					pid;
+	struct sigaction	sa;
+
+	pid = getpid();
+	ft_printf("server pid = %d\n", pid);
+	sa.sa_handler = handle_signal;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		pause();
+	}
+	return (0);
+}
